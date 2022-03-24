7d6896de58ca863d35f567b07a6e8b5cb1c7049b
anton.dosov <dosantappdev@gmail.com>
2022-03-24 21:52:59 +0100

    report twillio issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as Video from 'twilio-video'
+
+console.log(Video)
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
 }
